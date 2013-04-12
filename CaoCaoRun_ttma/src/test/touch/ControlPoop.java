3a43dced17893579847b7f944fe4e61919e0296b
diep.soanh@yahoo.com <diep.soanh@yahoo.com@f77c299e-e548-38b3-2df5-f6264ab8b1de>
2013-04-12 13:33:21 +0000

    Refactoring ControlPoop.class

--- CaoCaoRun_ttma/src/test/touch/ControlPoop.java
+++ CaoCaoRun_ttma/src/test/touch/ControlPoop.java
@@ -4,6 +4,8 @@ import MazeFactory.Cell;
 
 public class ControlPoop {
 	
+	private static float limitSin = 0.71f; 
+	
 	public int goTo(float x_end, float x_start,
 			float y_end, float y_start, Cell currentCell){
 		int goTo=-1;
@@ -37,7 +39,6 @@ public class ControlPoop {
 			case 3:
 				if(currentCell.isNorthWall()) nextGoTo=2;
 				else nextGoTo=0;
-//				break;
 			}
 		}
 		else{
@@ -78,53 +79,56 @@ public class ControlPoop {
 		else return false;
 	}
 	
-	private int findPoopWay(float _delx, float _dely){
-		int goTo=-1;
+	private float calculateSin(float _delx, float _dely) {
 		float canh_huyen = (float) Math.sqrt(_delx*_delx + _dely*_dely);
 		float sin_now = (float)(_dely/canh_huyen);
 		
+		return sin_now;
+	}
+	
+	private int findPoopWay(float _delx, float _dely){
+		int goTo=-1;
+
 		//Don't know why direction is reverse ???
-		if(moveNorth(_delx, _dely, sin_now)) goTo=2;
-		if(moveSouth(_delx, _dely, sin_now)) goTo=0;
-		if(moveWest(_delx, _dely, sin_now)) goTo=3;
-		if(moveEast(_delx, _dely, sin_now)) goTo=1;
+		if(moveNorth(_delx, _dely, calculateSin(_delx, _dely))) goTo=2;
+		if(moveSouth(_delx, _dely, calculateSin(_delx, _dely))) goTo=0;
+		if(moveWest(_delx, _dely, calculateSin(_delx, _dely))) goTo=3;
+		if(moveEast(_delx, _dely, calculateSin(_delx, _dely))) goTo=1;
 		
 		return goTo;
-
-		
 	}
 	
 	private boolean moveNorth(float _delx, float _dely, float _sinx){
-		if(_delx>0 && _dely>0 && _sinx>0.71)
+		if(_delx>0 && _dely>0 && _sinx>limitSin)
 			return true;
-		if(_delx<0 && _dely>0 && _sinx>0.71)
+		if(_delx<0 && _dely>0 && _sinx>limitSin)
 			return true;
 		
 		return false;
 	}
 	
 	private boolean moveSouth(float _delx, float _dely, float _sinx){
-		if(_delx<0 && _dely<0 && _sinx<(-0.71))
+		if(_delx<0 && _dely<0 && _sinx<(0.0f - limitSin))
 			return true;
-		if(_delx>0 && _dely<0 && _sinx<(-0.71))
+		if(_delx>0 && _dely<0 && _sinx<(0.0f - limitSin))
 			return true;
 		
 		return false;
 	}
 	
 	private boolean moveWest(float _delx, float _dely, float _sinx){
-		if(_delx<0 && _dely>0 && _sinx<(0.71))
+		if(_delx<0 && _dely>0 && _sinx<(limitSin))
 			return true;
-		if(_delx<0 && _dely<0 && _sinx>(-0.71))
+		if(_delx<0 && _dely<0 && _sinx>(0.0f - limitSin))
 			return true;
 		
 		return false;
 	}
 	
 	private boolean moveEast(float _delx, float _dely, float _sinx){
-		if(_delx>0 && _dely>0 && _sinx<(0.71))
+		if(_delx>0 && _dely>0 && _sinx<(limitSin))
 			return true;
-		if(_delx>0 && _dely<0 && _sinx>(-0.71))
+		if(_delx>0 && _dely<0 && _sinx>(0.0f - limitSin))
 			return true;
 		
 		return false;
